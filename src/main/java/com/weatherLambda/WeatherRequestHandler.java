afa692197b66ec1c1c151e886e9825e3b1d31231
haochiencho@gmail.com <haochiencho@gmail.com>
2018-12-26 23:54:42 -0800

    feat: grabs html from target urls. added corresponding maven dependencies. modified maven build plugin to include dependencies in JAR

--- src/main/java/com/weatherLambda/WeatherRequestHandler.java
+++ src/main/java/com/weatherLambda/WeatherRequestHandler.java
@@ -2,16 +2,37 @@ package com.weatherLambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 
 /**
  * Created by briancho on 12/23/18.
  */
 public class WeatherRequestHandler implements RequestHandler<RequestClass, ResponseClass> {
     public ResponseClass handleRequest(RequestClass request, Context context) {
-        context.getLogger().log("Input: " + request.getRequest());
-
         ResponseClass response = new ResponseClass();
-        response.setResponse("Hello World - " + request.getRequest());
+
+        try {
+            URL url = new URL("http://www.example.com/");
+            URLConnection connection = url.openConnection();
+            InputStream in = connection.getInputStream();
+
+            String encoding = connection.getContentEncoding();
+            encoding = encoding == null ? "UTF-8" : encoding;
+
+            String body = IOUtils.toString(in, encoding);
+            response.setResponse(body);
+        } catch (MalformedURLException badUrlException) {
+            response.setResponse("Malformed URL: " + badUrlException.toString());
+        } catch (IOException ioException) {
+            response.setResponse("IOException: " + ioException.toString());
+        }
 
         return response;
     }
